62a0a90f774e67ca63ddace490d2eafbd47628be
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-08 15:12:53 +0200

    Initial commit

---
+++ src/test/java/at/bojana/menagment/MenagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bojana.menagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
