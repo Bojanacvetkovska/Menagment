26fd7abcb7f9c298fa2e900b3e86458d6aec521f
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-08 15:40:39 +0200

    Add Menagment and MenagmentConroller

---
+++ src/main/java/at/bojana/menagment/Menagment.java
@@ -0,0 +1,19 @@
+package at.bojana.menagment;
+
+import java.awt.*;
+
+import static java.awt.Color.RED;
+import static java.awt.Color.green;
+
+public class Menagment {
+    public static Color getBlock(Color colors){
+        if (RED.equals(colors)) {
+            System.out.println("Red");
+            ;
+        }
+        return green;
+    }
+
+        }
+
+        
---
+++ src/main/java/at/bojana/menagment/MenagmentController.java
@@ -0,0 +1,4 @@
+package at.bojana.menagment;
+
+public class MenagmentController {
+}
