cf57b6e7e6f28746b4ebc38d85ec315d802be72b
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-08 16:43:25 +0200

    Add Test

--- src/main/java/at/bojana/menagment/Menagment.java
+++ src/main/java/at/bojana/menagment/Menagment.java
@@ -1,10 +1,13 @@
 package at.bojana.menagment;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.awt.*;
 
 import static java.awt.Color.RED;
 import static java.awt.Color.green;
-
+@RequestMapping
 public class Menagment {
     public static Color getBlock(Color colors){
         if (RED.equals(colors)) {
