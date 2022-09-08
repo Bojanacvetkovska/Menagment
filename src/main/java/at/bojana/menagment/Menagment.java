daa38442f1ecd91ff3f93528d887e1eb63b3c334
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-08 16:40:30 +0200

    Update Menagment.java

--- src/main/java/at/bojana/menagment/Menagment.java
+++ src/main/java/at/bojana/menagment/Menagment.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 import static java.awt.Color.BLUE;
 import static java.awt.Color.orange;
-
+@RequestMapping("/blue2orange")
 public class Menagment {
     public static Color getBlock(Color color) {
         if (BLUE.equals(color)) {
