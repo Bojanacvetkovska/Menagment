97434f37e0c631a8ab8e3ddec50027b7d5eb6708
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-08 16:36:54 +0200

    Add Menagment and MenagmentConroller

---
+++ src/test/java/at/bojana/menagment/Test.java
@@ -0,0 +1,14 @@
+package at.bojana.menagment;
+import .org.junit.jupiter.api.Test
+public class Test {
+    MenagmentApplication menagmentApplication= new MenagmentApplication();
+    @Test
+    void colorTest(){
+        if (String Object red = red);
+            Object green;
+            return green;
+        }
+
+    }
+
+
